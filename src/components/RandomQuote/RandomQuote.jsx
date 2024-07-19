e35430f518cd8680a339f413ba10acc22bc2d83b
Nemitha Wijerathna <nemithan05@gmail.com>
2024-07-19 12:05:04 +0530

    Randomquote component customize

--- src/components/RandomQuote/RandomQuote.jsx
+++ src/components/RandomQuote/RandomQuote.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
 import './RandomQuote.css';
+import twitter_icon from '../assets/icons/twitter.png';
+import reload_icon from '../assets/icons/reload.png';
 
 const RandomQuote = () => {
   const [quote, setQuote] = useState({
-    text: 'Did you know that the first computer programmer',
+    text: 'Did you know that the first computer programmer?',
     author: 'Ada Lovelace',
   });
 
   return (
-    <div classname='container'>
-      hi
+    <div className='container'>
+      <div className='quote'>{quote.text}</div>
+      <div>
+        <div className='line'></div>
+        <div className='bottom'>
+          <div className='author'>{quote.author}</div>
+          <div className='icons'>
+            <img src={reload_icon} alt=''/>
+            <img src={twitter_icon} alt=''/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
