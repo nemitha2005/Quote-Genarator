5b6cc7e1b2047edcf3daf80b7417d09cf89b8d9d
Nemitha Wijerathna <nemithan05@gmail.com>
2024-07-20 13:57:04 +0530

    api functions

--- src/components/RandomQuote/RandomQuote.jsx
+++ src/components/RandomQuote/RandomQuote.jsx
@@ -1,29 +1,43 @@
-import React, { useState } from 'react';
-import './RandomQuote.css';
-import twitter_icon from '../assets/icons/twitter.png';
-import reload_icon from '../assets/icons/reload.png';
+import React, { useState } from "react";
+import "./RandomQuote.css";
+import twitter_icon from "../assets/icons/twitter.png";
+import reload_icon from "../assets/icons/reload.png";
 
 const RandomQuote = () => {
+  let quotes = [];
+
+  async function loadQuotes() {
+    const response = await fetch("https://type.fit/api/quotes");
+    quotes = await response.json();
+  }
+
+  const random = () => {
+    const select = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(select);
+  }
+
   const [quote, setQuote] = useState({
-    text: 'Did you know that the first computer programmer?',
-    author: 'Ada Lovelace',
+    text: "Did you know that the first computer programmer?",
+    author: "Ada Lovelace",
   });
 
+  loadQuotes();
+
   return (
-    <div className='container'>
-      <div className='quote'>{quote.text}</div>
+    <div className="container">
+      <div className="quote">{quote.text}</div>
       <div>
-        <div className='line'></div>
-        <div className='bottom'>
-          <div className='author'>{quote.author}</div>
-          <div className='icons'>
-            <img src={reload_icon} alt=''/>
-            <img src={twitter_icon} alt=''/>
+        <div className="line"></div>
+        <div className="bottom">
+          <div className="author">{quote.author}</div>
+          <div className="icons">
+            <img src={reload_icon} onClick={()=>{random()}} alt="" />
+            <img src={twitter_icon} alt="" />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default RandomQuote;
